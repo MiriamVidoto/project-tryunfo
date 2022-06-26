7bd9397500fe6773adb29f82efaee9f0f72489da
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-06-26 19:24:03 -0300

    até o requisito 5, caminhando...

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ class App extends React.Component {
   state = {
     cardName: '',
     cardDescription: '',
-    cardAttr1: 0,
-    cardAttr2: 0,
-    cardAttr3: 0,
+    cardAttr1: '0',
+    cardAttr2: '0',
+    cardAttr3: '0',
     cardImage: '',
     cardRare: '',
     cardTrunfo: false,
@@ -19,23 +19,31 @@ class App extends React.Component {
 enableButton = () => {
   const {
     cardName,
-    // cardDescription,
-    // cardAttr1,
-    // cardAttr2,
-    // cardAttr3,
-    // cardImage,
-    // cardRare,
+    cardDescription,
+    cardAttr1,
+    cardAttr2,
+    cardAttr3,
+    cardImage,
+    cardRare,
   } = this.state;
 
-  // const datas = cardName !== ''
-  // && cardDescription !== ''
-  // && cardImage !== ''
-  // && cardRare !== '';
+  const validateValues = cardName !== ''
+  && cardDescription !== ''
+  && cardImage !== ''
+  && cardRare !== '';
 
-  if (cardName === miriam) {
-    this.setState({
-      isSaveButtonDisabled: false,
-    });
+  const maxSum = 210;
+  const max = 90;
+
+  const validateAttr = [cardAttr1, cardAttr2, cardAttr3]
+    .every((attr) => attr > 0 && attr <= max);
+
+  const sumAttr = (Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= maxSum);
+
+  if (validateValues && validateAttr && sumAttr) {
+    this.setState({ isSaveButtonDisabled: false });
+  } else {
+    this.setState({ isSaveButtonDisabled: true });
   }
 }
 
@@ -44,7 +52,7 @@ handleChange = ({ target }) => {
   const value = type === 'checkbox' ? target.checked : target.value;
   this.setState({
     [name]: value,
-  });
+  }, () => this.enableButton());
 }
 
 render() {
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -116,6 +116,7 @@ class Form extends React.Component {
               value={ cardRare }
               onChange={ onInputChange }
             >
+              <option value="" disabled selected>Selecione...</option>
               <option value="normal">normal</option>
               <option value="raro">raro</option>
               <option value="muito raro">muito raro</option>
